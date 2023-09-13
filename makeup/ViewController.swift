dc48a1bed2fd77399a993438976c01abfd0684a9
HiraYuki <g22908hs@gm.tsuda.ac.jp>
2023-09-13 16:23:51 +0900

    9-13 add new code

--- makeup/ViewController.swift
+++ makeup/ViewController.swift
@@ -15,6 +15,22 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func tapURL(_ sender: Any) {
+      
+       // let url = NSURL(string: "https://www.google.com/?hl=ja")
+              // if UIApplication.shared.canOpenURL(url! as URL) {
+                 //  UIApplication.shared.open(url! as URL, options: [:], completionHandler: nil)
+             //  }
+        
+        if let url = URL(string: "https://www.youtube.com/watch?v=L6o-D99sY0Q") {
+            if UIApplication.shared.canOpenURL(url) {
+                UIApplication.shared.open(url, options: [:], completionHandler: nil)
+            }
+        }
 
+       
+    }
+    
+    
 }
 
